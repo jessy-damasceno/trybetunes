f72f6abdf7d822827031265040e8ef1238d678d2
Jessy Damasceno <jessy_bas@hotmail.com>
2022-05-18 18:44:20 -0300

    add req4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 
 class Header extends React.Component {
@@ -21,6 +22,17 @@ class Header extends React.Component {
     return (!isLoading ? <h3>Carregando...</h3>
       : (
         <header data-testid="header-component">
+          <nav>
+            <ul>
+              <li><Link data-testid="link-to-search" to="/search">Search</Link></li>
+              <li>
+                <Link data-testid="link-to-favorites" to="/favorites">
+                  Músicas Favoritas
+                </Link>
+              </li>
+              <li><Link data-testid="link-to-profile" to="/profile">Profile</Link></li>
+            </ul>
+          </nav>
           <span data-testid="header-user-name">{name}</span>
         </header>
       )
