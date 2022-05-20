fa98c28098046da2f2061482f80dfcc0e00b8e85
Jessy Damasceno <jessy_bas@hotmail.com>
2022-05-20 16:48:53 -0300

    add req13

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,12 +1,44 @@
 import React from 'react';
 import Header from '../components/Header';
+// import Loading from '../components/Loading';
+import MusicCard from '../components/MusicCard';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 class Favorites extends React.Component {
+  state = {
+    // isLoading: false,
+    favoriteSongsList: [],
+  }
+
+  componentDidMount() {
+    this.getFavoriteSongsList();
+  }
+
+  getFavoriteSongsList = async () => {
+    // this.setState({ isLoading: false });
+    const favoriteSongsList = await getFavoriteSongs();
+    this.setState({
+      // isLoading: true,
+      favoriteSongsList,
+    });
+  }
+
   render() {
+    // const { favoriteSongsList, isLoading } = this.state;
+    const { favoriteSongsList } = this.state;
+    console.log(favoriteSongsList);
     return (
       <div data-testid="page-favorites">
         <Header />
-        <p>Favorites</p>
+        {favoriteSongsList.map(({ trackName, previewUrl, trackId }, index) => ((
+          <MusicCard
+            key={ index }
+            trackName={ trackName }
+            previewUrl={ previewUrl }
+            trackId={ trackId }
+            isFavorite
+          />
+        )))}
       </div>
     );
   }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,54 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import { getUser } from '../services/userAPI';
 
 class Profile extends React.Component {
+  state = {
+    isLoading: false,
+    user: {},
+  }
+
+  componentDidMount() {
+    this.getUserDetails();
+  }
+
+  async getUserDetails() {
+    this.setState({ isLoading: true });
+    const user = await getUser();
+    this.setState({ isLoading: false, user });
+  }
+
   render() {
+    const { user, isLoading } = this.state;
+    const { description, email, image, name } = user;
+    console.log(user);
     return (
       <div data-testid="page-profile">
         <Header />
-        <p>Profile</p>
+        {isLoading ? <Loading /> : (
+          <div className="userDetails">
+            <section>
+              <img data-testid="profile-image" src={ image } alt="Imagem de perfil" />
+              <Link to="/profile/edit">
+                Editar perfil
+              </Link>
+            </section>
+            <section className="profile_name">
+              <h3>Nome</h3>
+              <p>{name}</p>
+            </section>
+            <section className="profile_email">
+              <h3>E-mail</h3>
+              <p>{email}</p>
+            </section>
+            <section className="profile_description">
+              <h3>Descrição</h3>
+              <p>{description}</p>
+            </section>
+          </div>
+        )}
       </div>
     );
   }
