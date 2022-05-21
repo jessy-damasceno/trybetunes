2abcc8f0a40a6a63af6bd3d813878feb7547f771
Jessy Damasceno <jessy_bas@hotmail.com>
2022-05-21 00:07:15 -0300

    finalizando all reqs

--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import Loading from '../components/Loading';
 import { getUser, updateUser } from '../services/userAPI';
