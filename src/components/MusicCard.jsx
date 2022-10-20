5183584dcee99e12572eee7a10fbee94f7bd3e77
Jessy Damasceno <jessy_bas@hotmail.com>
2022-10-20 14:34:09 -0300

    change MusicCard component

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -15,7 +15,6 @@ class MusicCard extends React.Component {
       isLoading: true,
     });
     if (checked) {
-      // const getMusic = await getMusics(trackId);
       await addSong(musica);
       await getFavoriteSongsList();
     } else {
